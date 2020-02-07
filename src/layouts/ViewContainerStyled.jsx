ac54f34f0efc1def95b86bf239989929d7cb276b
publizm <publizm@gmail.com>
2020-02-07 21:56:01 +0900

    docs: added css mediaquery

--- src/layouts/ViewContainerStyled.jsx
+++ src/layouts/ViewContainerStyled.jsx
@@ -1,8 +1,22 @@
 import styled from 'styled-components';
+import media from '../libs/MediaQuery';
 
 export const ViewContainerLayout = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 400px 0 0;
   background: url('/images/test3.jpg') 0 0 no-repeat fixed;
+
+
+  /* desktop */
+  ${media.desktop`
+  `}
+
+  /* tablet */
+  ${media.tablet`
+  `}
+
+  /* mobile */
+  ${media.mobile`
+  `}
 `;
