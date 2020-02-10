2afbf180ce4a151c6589458725a62efb1f5e7f6a
kwansikdev <kwansk0424@gmail.com>
2020-02-10 10:19:52 +0900

    feat: Add modal components

---
+++ src/components/Header/CountryModalContents.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import * as S from './headerStyled';
+import { language } from './data';
+
+const CountryModalContents = ({ selectLang, setSelectLang, closeClick }) => {
+  const recommandLang = language.filter(item =>
+    selectLang.Id === 'KR'
+      ? item.Id === 'UK' || item.Id === 'US'
+      : selectLang.Id === 'UK'
+      ? item.Id === 'US' || item.Id === 'KR'
+      : item.Id === 'UK' || item.Id === 'KR',
+  );
+
+  const sortLang = [
+    selectLang,
+    ...language
+      .filter(item => selectLang.Id !== item.Id)
+      .sort((a, b) => {
+        return a.lang < b.lang ? -1 : a.lang > b.lang ? 1 : 0;
+      }),
+  ];
+
+  const clickLang = item => {
+    setSelectLang(item);
+    closeClick();
+  };
+
+  return (
+    <>
+      <S.ModalSection>
+        <S.ModalSectionTitle>추천 언어 및 지역</S.ModalSectionTitle>
+        <S.ModalContentUl>
+          {recommandLang.map((item, i) => (
+            <S.ModalContentsLi key={i}>
+              <S.ModalContentsButton
+                onClick={() => clickLang(item)}
+                status={selectLang.Id === item.Id}
+              >
+                <p>{item.lang}</p>
+                <p>{item.country}</p>
+              </S.ModalContentsButton>
+            </S.ModalContentsLi>
+          ))}
+        </S.ModalContentUl>
+        <S.ModalSectionTitle>언어와 지역을 선택해주세요</S.ModalSectionTitle>
+        <S.ModalContentUl>
+          {sortLang.map((item, i) => (
+            <S.ModalContentsLi key={i}>
+              <S.ModalContentsButton
+                onClick={() => clickLang(item)}
+                status={selectLang.Id === item.Id}
+              >
+                <p>{item.lang}</p>
+                <p>{item.country}</p>
+              </S.ModalContentsButton>
+            </S.ModalContentsLi>
+          ))}
+        </S.ModalContentUl>
+      </S.ModalSection>
+    </>
+  );
+};
+
+export default CountryModalContents;
