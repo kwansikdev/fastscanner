bd65de2b5723ea8ca513aeeede83b8b0d4f407b6
publizm <publizm@gmail.com>
2020-02-14 21:51:43 +0900

    refactor: ui calander responsive styling

--- src/components/SearchArea/SelectDate.jsx
+++ src/components/SearchArea/SelectDate.jsx
@@ -6,6 +6,9 @@ import koLocale from 'moment/locale/ko';
 import 'react-dates/lib/css/_datepicker.css';
 import './Date.css';
 import * as S from './SearchAreaStyled';
+import styled from 'styled-components';
+
+const DateRangeWrapper = styled.div``;
 
 const SelectDate = ({
   way,
@@ -50,28 +53,35 @@ const SelectDate = ({
   return (
     <fieldset className="option-field date">
       <S.FieldTitle>가는날 / 오는날</S.FieldTitle>
-      <DateRangePicker
-        startDateId="startDate"
-        endDateId="endDate"
-        startDate={momentOutDate}
-        endDate={way === 'round' ? momentInDate : null}
-        endDatePlaceholderText={way === 'oneway' ? '(편도)' : '입국날짜'}
-        onDatesChange={({ startDate, endDate }) => {
-          setStartDate(startDate);
-          setEndDate(endDate);
-        }}
-        focusedInput={focusedInput}
-        onFocusChange={focusedInput => {
-          setFocusedInput(focusedInput);
-        }}
-        disabled={way === 'oneway' ? 'endDate' : null}
-        numberOfMonths={1}
-        required={true}
-        displayFormat="YYYY년 MM월 DD일"
-        hideKeyboardShortcutsPanel={true}
-        noBorder={true}
-        readOnly={true}
-      />
+      <DateRangeWrapper
+        className={
+          focusedInput === 'startDate' ? 'openStartDate' : 'openEndDate'
+        }
+      >
+        <DateRangePicker
+          startDateId="startDate"
+          endDateId="endDate"
+          startDate={momentOutDate}
+          endDate={way === 'round' ? momentInDate : null}
+          endDatePlaceholderText={way === 'oneway' ? '(편도)' : '입국날짜'}
+          onDatesChange={({ startDate, endDate }) => {
+            setStartDate(startDate);
+            setEndDate(endDate);
+          }}
+          focusedInput={focusedInput}
+          onFocusChange={focusedInput => {
+            console.log(focusedInput);
+            setFocusedInput(focusedInput);
+          }}
+          disabled={way === 'oneway' ? 'endDate' : null}
+          numberOfMonths={1}
+          required={true}
+          displayFormat="YYYY년 MM월 DD일"
+          hideKeyboardShortcutsPanel={true}
+          noBorder={true}
+          readOnly={true}
+        />
+      </DateRangeWrapper>
     </fieldset>
   );
 };
